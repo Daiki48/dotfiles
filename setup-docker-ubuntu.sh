5001b0022a857614c3e0a4bef3498ae27addc69e
Daiki48 <daiki48.engineer@gmail.com>
2023-05-08 16:19:54 +0900

    :wrench: Added node setup & symbolic link

--- setup-docker-ubuntu.sh
+++ setup-docker-ubuntu.sh
@@ -1,3 +1,6 @@
+# This script file is using at my https://github.com/Daiki48/Dockerfiles.git.
+
+
 # Check arg 
 if [ "$1" = "-y" ]; then
   non_interactive=true
@@ -5,22 +8,6 @@ else
   non_interactive=false
 fi
 
-echo " -------------------------------------- "
-echo " ############# init setup ############# "
-echo " -------------------------------------- "
-
-printf "Install curl & wget & git & ripgrep & unzip & vim ..."
-
-apt update
-apt-get update
-apt install -y curl git ripgrep unzip vim wget
-
-printf "Successfully installed curl & wget & git & ripgrep & unzip & vim !!"
-
-echo " --------------------------------------------- "
-echo " ############# init setup finish ############# "
-echo " --------------------------------------------- "
-echo ""
 echo " ---------------------------------------- "
 echo " ############# neovim setup ############# "
 echo " ---------------------------------------- "
@@ -78,4 +65,68 @@ esac
 echo " ---------------------------------------------- "
 echo " ############# deno setup finish! ############# "
 echo " ---------------------------------------------- "
+echo ""
+echo " -------------------------------------- "
+echo " ############# node setup ############# "
+echo " -------------------------------------- "
+if [ "$non_interactive" = true ]; then
+  Answer="y"
+else
+  printf "Do you want to install node? (Y/n) [y]: "
+  read Answer < /dev/tty
+  Answer=${Answer:-y}
+fi
+
+case ${Answer} in
+  y|Y) 
+
+    echo "Install node..."
+
+    curl -fsSL https://deb.nodesource.com/setup_18.x | bash - &&\
+    apt install -y nodejs && \
+    npm install -g vim-language-server typescript-language-server
+
+    echo "Successfully installed!!" ;;
+  n|N)
+    echo "Skipped installation." ;;
+
+esac
+
+echo " ---------------------------------------------- "
+echo " ############# node setup finish! ############# "
+echo " ---------------------------------------------- "
+echo ""
+echo " ------------------------------------------------- "
+echo " ############# Create symbolic links ############# "
+echo " ------------------------------------------------- "
+if [ "$non_interactive" = true ]; then
+  Answer="y"
+else
+  printf "Do you want to create a symbolic link? (Y/n) [y]: "
+  read Answer < /dev/tty
+  Answer=${Answer:-y}
+fi
+
+case ${Answer} in
+  y|Y) 
+
+    echo "Create symbolic links..."
+
+    rm ~/.bashrc
+    rm ~/.config/nvim/init.vim
+    ln -s /dotfiles/.bashrc ~/.bashrc
+    ln -s /dotfiles/.config/nvim ~/.config/
+#     ln -s /dotfiles/.local/share/dein/toml ~/.local/share/dein/toml
+#     ln -s /dotfiles/.local/share/dein/plugins ~/.local/share/dein/plugins
+
+    echo "Successfully created!!" ;;
+  n|N)
+    echo "Skipped creation." ;;
+
+esac
+
+echo " --------------------------------------------------------- "
+echo " ############# Create symbolic links finish! ############# "
+echo " --------------------------------------------------------- "
+
 
