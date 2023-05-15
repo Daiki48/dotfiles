b3c1e89b385cf1b131ca473538b55c8bc760b526
Daiki48 <daiki48.engineer@gmail.com>
2023-05-16 08:41:09 +0900

    :wrench: Added keymap config

--- .config/nvim/lua/daiki/init.lua
+++ .config/nvim/lua/daiki/init.lua
@@ -1,3 +1,4 @@
 require('daiki.options.disable') -- default plugin disable
 require('daiki.options.base') -- base options
 require('daiki.dein') -- dein setting
+require('daiki.options.keymap') -- default keymap
