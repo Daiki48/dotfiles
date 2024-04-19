7a08fd603611835d7ea187940bdc315f581a6878
Daiki48 <daiki48.engineer@gmail.com>
2024-04-19 21:11:18 +0900

    :+1: Added codesnap.nvim plugin

--- .config/nvim/lua/daiki/plugins/init.lua
+++ .config/nvim/lua/daiki/plugins/init.lua
@@ -198,4 +198,24 @@ return {
 			'williamboman/mason.nvim',
 		}
 	},
+
+	-- codesnap
+	{
+		"mistricky/codesnap.nvim",
+		build = "make",
+		cmd = {'CodeSnap', 'CodeSnapSave'},
+		config = function ()
+			require("codesnap").setup({
+				mac_window_bar = true,
+				title = "",
+				code_font_family = "CaskaydiaCove Nerd Font",
+				watermark_font_family = "Pacifico",
+				watermark = "Dnfolio",
+				bg_theme = "bamboo",
+				breadcrumbs_separator = "/",
+				has_breadcrumbs = false,
+				save_path = "~/codesnap/out.png"
+			})
+		end
+	},
 }
