02191a2148dcb0ac11935cdad74f95f3a27755c7
Daiki48 <daiki48.engineer@gmail.com>
2024-05-30 18:00:04 +0900

    :hammer: Added visual block mode keymap

--- .config/nvim/lua/daiki/base/keymap.lua
+++ .config/nvim/lua/daiki/base/keymap.lua
@@ -4,3 +4,7 @@ local keymap = vim.api.nvim_set_keymap
 keymap('n', '<C-l>', '<cmd>bnext<cr>', { noremap = true, silent = true })
 keymap('n', '<C-h>', '<cmd>bprevious<cr>', { noremap = true, silent = true })
 keymap('n', '<C-q>', '<cmd>bd<cr>', { noremap = true })
+
+-- Visual block mode
+keymap('n', '<C-a>', '<C-v>', { noremap = true })
+keymap('v', '<C-a>', '<C-v>', { noremap = true })
