14fadc1adb335bc7e080e67972194c11b5f9525d
Daiki48 <daiki48.engineer@gmail.com>
2024-05-31 21:32:12 +0900

    :+1: Added sakurajima theme.

--- .config/nvim/lua/daiki/plugins/lualine.lua
+++ .config/nvim/lua/daiki/plugins/lualine.lua
@@ -55,7 +55,7 @@ end
 lualine.setup({
 	options = {
 		icons_enabled = true,
-		--    theme = 'iceberg_dark',
+	  theme = 'sakurajima',
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = {},
