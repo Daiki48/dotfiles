4b0b09a6135e936792542e78fcc84ed2346208c7
Daiki48 <daiki48.engineer@gmail.com>
2024-11-11 23:13:04 +0900

    :hammer: Changed keymap for oil.nvim close

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -64,7 +64,7 @@ require("oil").setup({
     ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
     ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
     ["<C-p>"] = "actions.preview",
-    ["<C-c>"] = "actions.close",
+    ["q"] = "actions.close",
     ["<C-l>"] = "actions.refresh",
     ["-"] = "actions.parent",
     ["_"] = "actions.open_cwd",
