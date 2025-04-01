93cd7629e6415b08357d2716357a4448f28202d2
Daiki48 <daiki48.engineer@gmail.com>
2025-04-01 09:23:26 +0900

    :hammer: Change event

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,8 +1,6 @@
 return {
-
   "lewis6991/gitsigns.nvim",
-  event = "BufRead",
-  -- lazy = false,
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
     require("gitsigns").setup()
   end,
