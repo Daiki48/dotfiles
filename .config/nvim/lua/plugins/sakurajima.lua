0c9a207cd8adda3a4802a40d2e8378b1a12821b1
Daiki48 <daiki48.engineer@gmail.com>
2026-02-25 10:45:31 +0900

    :hammer: Update config for Public

--- .config/nvim/lua/plugins/sakurajima.lua
+++ .config/nvim/lua/plugins/sakurajima.lua
@@ -1,6 +1,6 @@
 return {
   "Daiki48/sakurajima.nvim",
-  dir = "/media/develop/neovim-plugin/sakurajima.nvim",
+  -- dir = "/media/develop/neovim-plugin/sakurajima.nvim",
   priority = 1000,
   lazy = false,
   config = function()
